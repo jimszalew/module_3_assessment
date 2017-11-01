b922c4290a394f86fc77fc5d34cbd4846b7e70d3
Jim Szalewski <jim.szalewski@gmail.com>
2017-11-01 09:55:27 -0600

    begins building api call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,10 @@
 class SearchController < ApplicationController
   def index
-    
+    search = params[:search]
+    @conn = Faraday.new(url: "https://api.bestbuy.com/v1/stores((area(80202,25)))?apiKey=a8k7nnexb87aa9hb3zzqbgf7&format=json") do |faraday|
+      faraday.adapter Faraday.default_adapter
+      # faraday.headers["apiKey"] = ENV['bestbuy_api_key']
+    end
+    require "pry"; binding.pry
   end
 end
