c9ad7648f0d85745bc099846e755d49a2769048d
Jim Szalewski <jim.szalewski@gmail.com>
2017-11-01 09:25:29 -0600

    first commit, feature test written, adding search to navbar

---
+++ spec/features/store_search_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe "User searches for best buy stores" do
+  context "by zipcode" do
+    it "and sees stores matching the search params" do
+      # When I visit "/"
+      visit "/"
+      # And I fill in a search box with "80202" and click "search"
+      fill_in :search, with: "80202"
+      click_on "Search for Stores"
+      # Then my current path should be "/search" (ignoring params)
+      expect(current_path).to eq("/search")
+      # And I should see stores within 25 miles of 80202
+      # And I should see a message that says "17 Total Stores"
+      expect(page).to have_content("17 Total Stores")
+      # And I should see exactly 10 results (There are 17 stores within 25 miles. We want to display 10 on this page and 7 on the next. Get the first page to work first from start to finish and worry about pagination later.)
+      expect(page).to have_css(".store")
+      # And I should see the long name, city, distance, phone number and store type for each of the 10 results
+      within(first(".store")) do
+        expect(page).to have_css(".long_name")
+        expect(page).to have_css(".city")
+        expect(page).to have_css(".distance")
+        expect(page).to have_css(".phone")
+      end
+    end
+  end
+end
