dd4e9f79ba94072a45fa5a200ee2a99e34796ec1
Jim Szalewski <jim.szalewski@gmail.com>
2017-11-01 10:41:30 -0600

    sets up storecreator poro, may not use it

--- app/services/store_creator.rb
+++ app/services/store_creator.rb
@@ -1,15 +1,13 @@
-class Store < ActiveRecord::Base
-  validates_presence_of :long_name, :city, :distance, :phone
-
-  attr_reader :long_name,
-              :city,
-              :distance,
-              :phone
-
+class StoreCreator
   def initialize(store_info)
     @long_name = store_info[:long_name]
     @city      = store_info[:city]
     @distance  = store_info[:distance]
     @phone     = store_info[:phone]
+    Store.create(
+      long_name: @long_name,
+      city: @city,
+      distance: @distance,
+      phone: @phone)
   end
 end
