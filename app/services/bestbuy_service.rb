332e713b8e9975352ee64dfc683f272815c7f0c0
Jim Szalewski <jim.szalewski@gmail.com>
2017-11-01 11:48:55 -0600

    final commit

--- app/services/bestbuy_service.rb
+++ app/services/bestbuy_service.rb
@@ -1,12 +1,5 @@
 class BestbuyService
   def initialize
-    @conn = Faraday.new("https://api.bestbuy.com") do |faraday|
-      faraday.adapter Faraday.default_adapter
-    end
-  end
-
-  def self.stores_by_zip(zipcode)
-    response = @conn.get("/v1/stores((area(#{zipcode},25)))?apiKey=#{ENV['bestbuy_api_key']}&format=json")
-    JSON.parse(response.body, symbolize_names: true)[:stores]
+    @conn = Faraday.new("https://api.bestbuy.com/v1/stores((area(#{zipcode},25)))?apiKey=#{ENV['bestbuy_api_key']}&format=json")
   end
 end
